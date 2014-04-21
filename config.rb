f089f0de852cb1fba6ff058c5b5f05befb33f679
David Watson <davidlwatsonjr@gmail.com>
2014-04-20 23:39:40 -0400

    More changes

--- config.rb
+++ config.rb
@@ -2,10 +2,11 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/blog/wp-content/themes/davidwritescode"
-css_dir = "stylesheets"
+css_dir = "."
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
+line_comments = false
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
