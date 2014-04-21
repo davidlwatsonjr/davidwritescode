b4e8c28051640d2c6965ed1b1eb3bd8e05343ca5
David Watson <davidlwatsonjr@gmail.com>
2014-04-20 21:48:56 -0400

    Some footer styling to match

--- footer.php
+++ footer.php
@@ -1,26 +1,27 @@
 			</div>
 			<!-- .content-wrapper -->
+
+			<footer class="footer">
+				<div class="footer-content">
+					<span class="random-message"><?php echo dwc_random_message(); ?></span>
+					<div class="right">
+						<a class="link-to-top" href="#top">Top</a>
+						<?php wp_nav_menu([
+							'fallback_cb' => 'dwc_page_menu_flat',
+							'container' => false,
+							'depth' => '1',
+							'theme_location' => 'secondary',
+							'link_before' => '',
+							'link_after' => ''
+						]); ?>
+						<?php dynamic_sidebar('footer-widget-area'); ?>
+					</div>
+				</div>
+			</footer>
+			<!-- .footer -->
+
 		</div>
 		<!-- .page-wrapper -->
-
-		<footer class="footer">
-			<div class="footer-content">
-				<span class="random-message"><?php echo dwc_random_message(); ?></span>
-				<div class="right">
-					<a class="link-to-top" href="#top">Top</a>
-					<?php wp_nav_menu([
-						'fallback_cb' => 'dwc_page_menu_flat',
-						'container' => false,
-						'depth' => '1',
-						'theme_location' => 'secondary',
-						'link_before' => '',
-						'link_after' => ''
-					]); ?>
-					<?php dynamic_sidebar('footer-widget-area'); ?>
-				</div>
-			</div>
-		</footer>
-		<!-- .footer -->
 
 		<?php wp_footer(); ?>
 <?php /*
