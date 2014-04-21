de7862f86ed3415cb1f7a0ee53faa4f2825a968c
David Watson <davidlwatsonjr@gmail.com>
2014-04-21 01:31:26 -0400

    Something funky with posts

--- template_parts/posts.php
+++ template_parts/posts.php
@@ -1,26 +1,28 @@
-<?php while (have_posts()) : the_post(); ?>
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header class="post-header">
-			<?php edit_post_link('Edit'); ?>
-			<h2 class="post-title">
-				<a rel="bookmark"
-				   href="<?php the_permalink() ?>"
-				   title="<?php the_title_attribute(); ?>">
-					<?php the_title(); ?>
-				</a>
-			</h2>
-			<?php get_template_part('template_parts/post-meta'); ?>
-			<?php the_tags( '<p class="post-tags">', ', ', '</p>'); ?>
-		</header>
+<div class="posts-listing">
+	<?php while (have_posts()) : the_post(); ?>
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header class="post-header">
+				<?php edit_post_link('Edit'); ?>
+				<h2 class="post-title">
+					<a rel="bookmark"
+					   href="<?php the_permalink() ?>"
+					   title="<?php the_title_attribute(); ?>">
+						<?php the_title(); ?>
+					</a>
+				</h2>
+				<?php get_template_part('template_parts/post-meta'); ?>
+			</header>
 
-		<div class="post-content">
-			<?php the_post_thumbnail(); ?>
-			<?php the_excerpt() ?>
-		</div>
+			<div class="post-content">
+				<?php the_post_thumbnail(); ?>
+				<?php the_excerpt() ?>
+			</div>
 
-		<footer class="post-footer">
-		</footer>
-	</article>
-<?php endwhile; ?>
+			<footer class="post-footer">
+				<?php the_tags( '<p class="post-tags">', ' &bull; ', '</p>'); ?>
+			</footer>
+		</article>
+	<?php endwhile; ?>
+</div>
 
 <?php get_template_part('template_parts/posts_navigation'); ?>
