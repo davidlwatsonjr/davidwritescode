54312ebe9f3433325ac1ef32a2aae8438b7d243a
David Watson <david@davidwritescode.com>
2014-07-11 20:23:58 -0600

    Change the stuff back to using /blog

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/wp-content/themes/davidwritescode"
+http_path = "/blog/wp-content/themes/davidwritescode"
 css_dir = "."
 sass_dir = "sass"
 images_dir = "images"
