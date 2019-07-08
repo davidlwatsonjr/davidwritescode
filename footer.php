17ae8416e623613f2fd252fcdab35dc37247758c
David Watson <git@davidlwatsonjr.com>
2019-07-07 23:52:30 -0400

    Remove unused social garbage

--- footer.php
+++ footer.php
@@ -24,32 +24,5 @@
         <!-- .page-wrapper -->
 
         <?php wp_footer(); ?>
-<?php /*
-        <div id="fb-root"></div>
-        <script>
-            (function(d, s, id){
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) return;
-                js = d.createElement(s); js.id = id;
-                js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-                fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-        </script>
-        <script>
-            (!function(d,s,id){
-                var js,fjs=d.getElementsByTagName(s)[0];
-                if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}
-            }(document,"script","twitter-wjs"));
-        </script>
-        <script>
-            (function(){
-                var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-                po.src = 'http://apis.google.com/js/plusone.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-            }());
-        </script>
-        <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4ff9369a70a5bb68"></script>
-
-*/ ?>
     </body>
 </html>
