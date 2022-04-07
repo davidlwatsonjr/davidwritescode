551dc2c2f5eebf200370d82ce48d0c8c9b34ee61
David Watson <david@davidlwatsonjr.com>
2022-04-06 22:27:26 -0400

    Remove unnecessary class

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form class="search-form" role="search" method="get" action="<?php echo home_url(); ?>/">
     <input class="field" type="search" name="s" value="<?php the_search_query(); ?>" placeholder="Search..." title="Search For:" />
-    <button class="btn search-btn dwc-search-btn" type="submit" title="Search"></button>
+    <button class="btn search-btn" type="submit" title="Search"></button>
 </form>
