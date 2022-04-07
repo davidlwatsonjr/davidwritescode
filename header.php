8c90bc38b1fb131aa619524ee3bf7d7361a1626d
David Watson <david@davidlwatsonjr.com>
2022-04-07 00:35:53 -0400

    Remove bootstrap since it's baked into our CSS now

--- header.php
+++ header.php
@@ -9,9 +9,6 @@
         <?php get_template_part('template_parts/site_meta'); ?>
         <?php get_template_part('template_parts/favicon'); ?>
 
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 
         <link rel="alternate" type="application/rss+xml" title="Subscribe to <?php bloginfo('name'); ?>" href="<?php bloginfo('rss2_url'); ?>" />
